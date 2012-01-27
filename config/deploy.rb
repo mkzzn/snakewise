980c76063ce4d29b6a0b2f536f4e4acd15b4e4c5
Mike Zazaian <mk@mkzzn.org>
2012-01-27 13:07:16 -0500

    some changes to deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,9 @@
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+require 'rvm/capistrano'
+require 'bundler/capistrano'
+
+set :bundle_flags,      "--deployment --verbose"
+
 set :application, "snakewise"
 set :user, "mz"
 set :port, 1440
@@ -7,21 +13,18 @@ set :repository,  "mkzzn_github:mkzzn/snakewise.git"
 set :deploy_to, "/var/www/#{application}"
 set :scm, :git
 
-require "bundler/capistrano"                    # Load bundler recipe
-
 default_run_options[:pty] = true
 
 $:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
-require "rvm/capistrano"                        # Load RVM's capistrano plugin.
 set :rvm_ruby_string, 'ruby-1.9.2-p290@global'   # Or whatever env you want it to run in.
 set :rvm_type, :user
 
 set :default_environment, {
-  'PATH' => "/home/mz/.rvm/gems/ruby-1.9.2-p290@global/bin:/home/mz/.rvm/rubies/ruby-1.9.2-p290/bin:/home/mz/.rvm/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games",
+  'PATH' => "/home/mz/.rvm/gems/ruby-1.9.2-p290@snakewise/bin:/home/mz/.rvm/rubies/ruby-1.9.2-p290/bin:/home/mz/.rvm/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games",
   'RUBY_VERSION' => 'ruby 1.9.2',
-  'GEM_HOME'    => "/home/mz/.rvm/gems/ruby-1.9.2-p290@global",
-  'GEM_PATH'    => "/home/mz/.rvm/gems/ruby-1.9.2-p290@global:/home/mz/.rvm/gems/ruby-1.9.2-p290@global",
-  'BUNDLE_PATH'    => "/home/mz/.rvm/gems/ruby-1.9.2-p290@global:/home/mz/.rvm/gems/ruby-1.9.2-p290@global"
+  'GEM_HOME'    => "/home/mz/.rvm/gems/ruby-1.9.2-p290@snakewise",
+  'GEM_PATH'    => "/home/mz/.rvm/gems/ruby-1.9.2-p290@snakewise:/home/mz/.rvm/gems/ruby-1.9.2-p290@snakewise",
+  'BUNDLE_PATH'    => "/home/mz/.rvm/gems/ruby-1.9.2-p290@snakewise:/home/mz/.rvm/gems/ruby-1.9.2-p290@snakewise"
 }
 
 #set :port, 1440                      # The port you've setup in the SSH setup section
